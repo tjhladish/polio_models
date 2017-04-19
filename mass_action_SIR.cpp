db6f90ff138f1eabdfafd25a3ce1ce8662fe8649
celestevallejo <cvallejo@ufl.edu>
2017-04-19 14:12:41 -0400

    Add files via upload

--- mass_action_SIR.cpp
+++ mass_action_SIR.cpp
@@ -1,29 +1,62 @@
 #include "EventDriven_MassAction_Sim.h"
 
-int main() {
-    int numSims = 1;
-    int N        = 5;
-    double GAMMA = 13;//1/(double) 6;  //***castes 6 to a double for double division
-    double BETA  = 200;//1/(double) 4;
-    double KAPPA = .8434;
-    double RHO = .02;
-    double BIRTH = .02;
-    double DEATH = .02;
-    
-    double vecSumm=0.0;
-    
-    for(int i=0; i<numSims; i++ ) {
-        EventDriven_MassAction_Sim sim(N,GAMMA,BETA,KAPPA,RHO,BIRTH,DEATH);
-        sim.randomizePopulation(1,1);//randomize with # of infecteds and # to vacc in parentheses
-        sim.printPeople();
-       // sim.rand_infect(1);
-        sim.runSimulation();
-        sim.printPeople();
-        //cout << sim.epidemic_size() << endl;
-      //vecSumm+=sim.FinalTime();
-    }
-    //cout<<"avg sim time: "<<vecSumm/numSims<<"\n";
 
+int main() {
+    int numSims = 50;
+    int N        = 100;
+    //double GAMMA = 13;
+    
+    //double KAPPA = .8434;
+    //double RHO = .02;
+    double BIRTH = .008;//Pakistan death rate per year
+    double DEATH = .008;
+    double BETA  = 15;
+    double BETAENV = 2;//contact environment once per day
+for(int j=0;j<10;j++){
+
+    double vecSumm=0.0;
+    double firstInfecSum=0.0;
+    vector<double> R0;
+    
+    for(int i=0; i<numSims; i++) {
+        EventDriven_MassAction_Sim sim(N,BETA,BIRTH,DEATH, BETAENV);
+        sim.randomizePopulation(1);//randomize with # of infecteds 
+        //sim.printPeople();
+        sim.runSimulation();
+        //sim.printPeople();
+        vecSumm+=sim.FinalTime();
+        firstInfecSum+=sim.avgFirstInfec();
+        R0.push_back(sim.R0());
+    }
+        double meanR0 = 0.0;
+        for(int k=0;k<R0.size();k++){
+            meanR0 += R0[k];
+            cout<<"in R0 loop\n";
+        }
+        cout<<"diff in avg R0 "<<((meanR0/numSims)-4)<<"\n";
+        if(abs((meanR0/numSims)-8)<.001){
+            BETA = BETA;
+            BETAENV=BETAENV;
+            cout<<"keeping same\n";
+        }
+        else{
+            BETA -= .8*((meanR0/numSims)-8);
+            BETAENV-= .2*((meanR0/numSims)-8);
+        }
+   // cout<<"avg sim time: "<<vecSumm/numSims<<"\n";
+   // cout<<"avg age at first infection: "<<firstInfecSum/numSims<<"\n";
+    //cout<<"avg num infected: "<<R0Sum/numSims<<"\n";
+   std::ofstream myfile1;
+    myfile1.open ("/Users/Celeste/Desktop/N=100_R0EST_10sims_test_7.csv");
+    myfile1<<vecSumm/numSims<<"\n";
+    myfile1<<firstInfecSum/numSims<<"\n";
+    for(int i=0;i<R0.size();i++){
+        myfile1<<R0[i]<<"\n";
+    }
+    myfile1.close();
+        cout<<"BETA "<<BETA<<"\n";
+        cout<<"BETAENV "<<BETAENV<<"\n";
+    }
 
     return 0;
 }
