94715337d3d3ee91b70d4bd93cdf3e9644a48df9
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2017-11-05 21:29:00 -0500

    added all integrals to calculate R0 and equilibrium values...possibly dirty commit...error message: failed to reach tolerance with highest-order rule...seems like a parameter issue

---
+++ integration_test.cpp
@@ -0,0 +1,360 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include <gsl/gsl_integration.h>
+
+// compiled on roc using:
+// g++ -O2 -std=c++11 -Wall -Wextra -Wno-deprecated-declarations --pedantic -I/home/tjhladish/work/AbcSmc/gsl_local/include/ integration_test2.cpp -o integ_test -lm -L/home/tjhladish/work/AbcSmc/gsl_local/lib/ -lgsl -lgslcblas -lpthread -ldl
+
+using namespace std;
+
+double integ_pi1(const double tau);
+double integ_pi2(const double tau);
+double integ_pi3(const double tau);
+double integ_gamma1pi1(const double tau);
+double integ_gamma2pi3(const double tau);
+double R0(double tau, void *params);
+double pi1(double tau, void *params);
+double pi2(double tau, void *params);
+double pi3(double tau,void *params);
+
+struct f_params {
+    double y0=1;
+    double b0=1;
+    double mu=0.318;
+    double mu0=.16;
+    double c=0.01;
+    double K=0.001;
+    double delta=0.02;
+    double contactRate1 =1;
+    double contactRate2 = 1;
+    double recover1=1;
+    double recover2=1;
+    double waneRate=1;
+    double y1=1;
+    double r=1.49;
+    double nu=1.69;
+    double t1=28;
+    double popSize = 10;
+    double T = 28;//infectivity time
+    double S = 120; //susceptiblility time
+
+};
+
+double beta1(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return p.contactRate1*(p.b0*exp(p.mu0*tau)-(p.c*p.y0*(exp(p.mu*tau)-exp(p.mu0*tau))/(p.mu-p.mu0)))/((p.b0*exp(p.mu0*tau)-(p.c*p.y0*(exp(p.mu*tau)-exp(p.mu0*tau))/(p.mu-p.mu0)))+p.K);
+}
+double gamma1(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return p.recover1*p.y0*exp(p.mu*tau)/(p.b0*exp(p.mu0*tau)-(p.c*p.y0*(exp(p.mu*tau)-exp(p.mu0*tau)/(p.mu-p.mu0)))+p.K);
+}
+double gamma2(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return p.recover2*p.y0*exp(p.mu*tau)/(p.b0*exp(p.mu0*tau)-(p.c*p.y0*(exp(p.mu*tau)-exp(p.mu0*tau)/(p.mu-p.mu0)))+p.K);
+}
+double gamma1pi1(double tau,void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return gamma1(tau,&p)*pi1(tau,&p);
+}
+double gamma2pi3(double tau,void *params){
+  f_params &p= *reinterpret_cast<f_params *>(params);
+  return gamma2(tau,&p)*pi3(tau,&p);
+}
+double pi1(double tau, void *params) {
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    //solved the integral below symbollically using mathematica
+    return exp(-(p.K*p.recover1*tau+tau*p.delta+(p.recover1*(tau*p.mu0-log(-p.c*exp(tau*p.mu)*p.y0+exp(tau*p.mu0)*(p.c*p.y0+p.b0*(p.mu-p.mu0)))+log(p.b0*(p.mu-p.mu0))))/p.c));
+}
+double eq_I1(double tau, void *params){
+    f_params &p = *reinterpret_cast<f_params *>(params);
+    return p.mu*p.popSize*(1-1/R0(tau,&p))*pi1(tau,&p);
+}
+double pi2(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    //approximate the function using a Taylor series and then integrate that
+    //integrated taylor series is summed below
+    double nTerm=0;
+    double taylorSum=0;
+    double tol = 1e-2;
+    int n =0;//series counter
+    
+    int factorial;
+    for(int n=0;n<5;n++){
+        if(n==0){
+            factorial=1;
+        }
+        else{
+            factorial*=n;
+        }
+    
+        taylorSum += pow(p.waneRate*p.mu,n)*pow(p.y1,n*p.r-1)*pow(p.nu,n)*pow(tau,n+1)/factorial;//this assumes concentration of antibodies is never zero
+    }
+    
+    
+    return exp(-taylorSum*(R0(tau,&p))+p.delta);
+}
+double eq_R(double tau,void *params){
+    f_params &p = *reinterpret_cast<f_params *>(params);
+    return (p.mu*p.popSize*(1-1/R0(tau,&p))*integ_gamma1pi1(p.T)+p.popSize*integ_gamma2pi3(p.T)*((1-1/R0(tau,&p))*(1-p.mu*(integ_pi1(p.T)+integ_gamma1pi1(p.T)*integ_pi2(p.S))))/integ_gamma2pi3(p.T)*integ_pi2(p.S)+integ_pi3(p.T))*pi2(tau,&p);
+}
+double pi3(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    //solved the integral below symbollically using mathematica
+    return exp(-(p.K*p.recover2*tau+tau*p.delta+(p.recover2*(tau*p.mu0-log(-p.c*exp(tau*p.mu)*p.y0+exp(tau*p.mu0)*(p.c*p.y0+p.b0*(p.mu-p.mu0)))+log(p.b0*(p.mu-p.mu0))))/p.c));
+}
+double eq_Ir(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return p.popSize*(1-1/R0(tau,&p))*(1-p.mu*(integ_pi1(p.T)+integ_gamma1pi1(p.T)*integ_pi2(p.S)))/(integ_gamma2pi3(p.T)*integ_pi2(p.S)+integ_pi3(p.T))*pi3(tau,&p);
+}
+
+double R0(double tau, void *params){
+    f_params &p= *reinterpret_cast<f_params *>(params);
+    return beta1(tau,&p)*pi1(tau,&p);
+}
+
+
+
+double integ_eqI1(const double tau) {
+    f_params params;
+    
+    
+    gsl_function F;
+    F.function = &eq_I1;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=1e-4;
+    const double epsrel=1e-4;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+double integ_eqR(const double tau) {
+  f_params params;
+  
+  
+  gsl_function F;
+  F.function = &eq_R;
+  F.params = reinterpret_cast<void *>(&params);
+  
+  double result, error;
+  size_t neval;
+  
+  const double xlow=0;
+  const double xhigh=tau;
+  const double epsabs=1e-4;
+  const double epsrel=1e-4;
+  
+  int code=gsl_integration_qng (&F,
+                                xlow,
+                                xhigh,
+                                epsabs,
+                                epsrel,
+                                &result,
+                                &error,
+                                &neval);
+  return result;
+}
+double integ_eqIr(const double tau) {
+    f_params params;
+    
+    
+    gsl_function F;
+    F.function = &eq_Ir;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=0;
+    const double epsrel=0;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+double integ_gamma1pi1(const double tau) {
+  f_params params;
+  
+  
+  gsl_function F;
+  F.function = &gamma1pi1;
+  F.params = reinterpret_cast<void *>(&params);
+  
+  double result, error;
+  size_t neval;
+  
+  const double xlow=0;
+  const double xhigh=tau;
+  const double epsabs=1e-4;
+  const double epsrel=1e-4;
+  
+  int code=gsl_integration_qng (&F,
+                                xlow,
+                                xhigh,
+                                epsabs,
+                                epsrel,
+                                &result,
+                                &error,
+                                &neval);
+  return result;
+}
+double integ_gamma2pi3(const double tau) {
+  f_params params;
+  
+  
+  gsl_function F;
+  F.function = &gamma2pi3;
+  F.params = reinterpret_cast<void *>(&params);
+  
+  double result, error;
+  size_t neval;
+  
+  const double xlow=0;
+  const double xhigh=tau;
+  const double epsabs=1e-4;
+  const double epsrel=1e-4;
+  
+  int code=gsl_integration_qng (&F,
+                                xlow,
+                                xhigh,
+                                epsabs,
+                                epsrel,
+                                &result,
+                                &error,
+                                &neval);
+  return result;
+}
+double integ_R0(const double tau) {
+    f_params params;
+    
+    gsl_function F;
+    F.function = &R0;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=1e-1;
+    const double epsrel=1e-1;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+double integ_pi1(const double tau) {
+    f_params params;
+    
+    
+    gsl_function F;
+    F.function = &pi1;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=1e-4;
+    const double epsrel=1e-4;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+double integ_pi2(const double tau) {
+    f_params params;
+    
+    gsl_function F;
+    F.function = &pi2;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=1e-1;
+    const double epsrel=1e-1;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+double integ_pi3(const double tau) {
+    f_params params;
+    
+    gsl_function F;
+    F.function = &pi3;
+    F.params = reinterpret_cast<void *>(&params);
+    
+    double result, error;
+    size_t neval;
+    
+    const double xlow=0;
+    const double xhigh=tau;
+    const double epsabs=1e-1;
+    const double epsrel=1e-1;
+    
+    int code=gsl_integration_qng (&F,
+                                  xlow,
+                                  xhigh,
+                                  epsabs,
+                                  epsrel,
+                                  &result,
+                                  &error,
+                                  &neval);
+    return result;
+}
+
+int main(void) {
+    const double tau_max = 28;
+    const double step = 1.0;
+    for (double tau = 0; tau < tau_max; tau += step){
+        double result = integ_eqIr(tau);
+        cout << "tau, Result " << tau << ", " << result << endl;
+        
+    }
+    
+}
