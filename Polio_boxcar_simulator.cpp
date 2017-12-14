a588f9e4484557b3250149851da5c837042f95fe
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2017-12-14 14:31:23 -0500

    fixed indexing issue

--- Polio_boxcar_simulator.cpp
+++ Polio_boxcar_simulator.cpp
@@ -26,8 +26,10 @@ int main(int argc, char** argv){
     //Determine infectious period for each infected compartment
     infectPeriod();
     
+    
     //Constuct model and parameters
-    Polio_boxcar_model model(0.02,numContacts*probInfection(1),numContacts*probInfection(10),numContacts*probInfection(100),numContacts*probInfection(1000),1/infectPeriod()[1],1/infectPeriod()[2],1/infectPeriod()[3],1/infectPeriod()[4],.003,.003,.003);
+    Polio_boxcar_model model(0.02,numContacts*probInfection(1),numContacts*probInfection(10),numContacts*probInfection(100),numContacts*probInfection(1000),1/infectPeriod()[0],1/infectPeriod()[1],1/infectPeriod()[2],1/infectPeriod()[3],.3,.3,.3);
+    
     
     //Initialize model using user inputs
     model.initialize(atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]),atoi(argv[7]),atoi(argv[8]),atoi(argv[9]));
