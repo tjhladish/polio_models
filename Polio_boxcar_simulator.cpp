1fb792d65bbd5533241921b821a0ac596e42629e
celestevallejo <Celeste@celestes-mbp.attlocal.net>
2017-12-20 21:35:58 -0500

    changed command line arguments, added vectors to hold parameters for constructor

--- Polio_boxcar_simulator.cpp
+++ Polio_boxcar_simulator.cpp
@@ -7,18 +7,24 @@
 //
 
 #include <stdio.h>
-#include "Polio_boxcar_model.h"
+#include "Polio_boxcar_model_extended_waning.h"
 #include "DIFFEQ_SIM.h"
 #include <math.h>
 #include "Polio_boxcar_parameters.h"
+#include <vector>
 
 void usage(){
-    cout<<"\n\t Usage: Input model initialization <S> <I1> <I2> <I3> <I4> <R1> <R2> <R3> <R4>\n";
+    cout<<"\n\t Usage: Input model initialization <S> <I1> <I2> <I3> <I4> <sum of all R compartments>\n";
     exit(-1);
 }
 
 int main(int argc, char** argv){
-    if(argc!= 10) usage();
+    if(argc!= 7) usage();
+    
+    vector<double> initialValues(argc-1);
+    for(int i = 1; i<argc; i++){
+        initialValues[i-1] = atof(argv[i]);
+    }
     
     //Determine peak antibody level for each infected compartment
     setPeakAnt();
@@ -26,13 +32,34 @@ int main(int argc, char** argv){
     //Determine infectious period for each infected compartment
     infectPeriod();
     
+    //set number of compartments
+    int nbins = 9; //total compartments
+    int sbins = 1;
+    int ibins = 4;
+    int rbins = nbins - (sbins + ibins);
+    
+    //vectors for parameters
+    vector<double> beta(sbins + rbins-1);//1 recovered compartment has complete immunity
+    vector<double> gamma(ibins);
+    vector<double> recovery(rbins);
+    
+    for(int i = 0; i < beta.size(); i++){
+        beta[i] = numContacts*probInfection(pow(10,i));
+    }
+    for(int i = 0; i < gamma.size(); i++){
+        gamma[i] = 1.0/infectPeriod()[i];
+    }
+    for(int i = 0; i < recovery.size(); i++){
+        recovery[i] = .03;
+    }
+    
     
     //Constuct model and parameters
-    Polio_boxcar_model model(0.02,numContacts*probInfection(1),numContacts*probInfection(10),numContacts*probInfection(100),numContacts*probInfection(1000),1/infectPeriod()[0],1/infectPeriod()[1],1/infectPeriod()[2],1/infectPeriod()[3],.3,.3,.3);
+    Polio_boxcar_model model(birthRate,beta,gamma,recovery,nbins);
     
     
     //Initialize model using user inputs
-    model.initialize(atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]),atoi(argv[7]),atoi(argv[8]),atoi(argv[9]));
+    model.initialize(initialValues, rbins);
     
     //Run model
     model.run_simulation();
